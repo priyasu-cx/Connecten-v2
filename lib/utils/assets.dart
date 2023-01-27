73f5acda62f875bfc68eb3682d5f1d0f76c7c894
shubhayu-64 <majumdarshubhayu@gmail.com>
2023-01-28 02:32:49 +0530

    zz

---
+++ lib/utils/assets.dart
@@ -0,0 +1,3 @@
+abstract class ImageAsset {
+  static const splashScreenImage = '';
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  static const googleGrey = Color(0xffe8eaed);
+}
