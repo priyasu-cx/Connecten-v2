7433a94322897d58d3f65132bff56cb33b004b4c
priyasu-cx <codexposer.priyasu25@gmail.com>
2023-01-28 02:46:02 +0530

    Some Changes

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColor {
-  static const googleGrey = Color(0xffe8eaed);
+  static const primarybgcolor = Color(0xffFFFFFF);
+  static const arrowcolor = Color(0xff22215B);
+  static const secbgcolor = Color(0xffF1F3F6);
+  static const cardcolor = Color(0xffEEF7FE);
 }
