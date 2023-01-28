30b99ccf832612fe3c4338ee13f4c361fb5e6333
shubhayu-64 <majumdarshubhayu@gmail.com>
2023-01-29 03:30:14 +0530

    z

--- lib/NearbyConnectScreen/nearby_connect.dart
+++ lib/NearbyConnectScreen/nearby_connect.dart
@@ -1,111 +1,111 @@
+import 'package:connecten/provider/connection_provider.dart';
+import 'package:connecten/utils/colors.dart';
+import 'package:connecten/view/Nav_Drawer/menu.dart';
+import 'package:connecten/view/profile_dialog.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hacknitr_round2/Models/user_models.dart';
-import 'package:hacknitr_round2/Providers/connection_provider.dart';
-import 'package:hacknitr_round2/Providers/database_provider.dart';
-import 'package:hacknitr_round2/Services/database_service.dart';
-import 'package:hacknitr_round2/utils/size_config.dart';
-import 'package:hacknitr_round2/widgets/appbar.dart';
-import 'package:hacknitr_round2/widgets/drawer.dart';
-import 'package:hacknitr_round2/widgets/profile_dialog.dart';
+import 'package:get/get.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../provider/sign_in_provider.dart';
+import 'package:provider/provider.dart';
 
-class NearbyConnect extends ConsumerStatefulWidget {
+class NearbyConnect extends StatefulWidget {
   const NearbyConnect({Key? key}) : super(key: key);
 
   @override
-  ConsumerState<NearbyConnect> createState() => _NearbyConnectState();
+  State<NearbyConnect> createState() => _NearbyConnectState();
 }
 
-class _NearbyConnectState extends ConsumerState<NearbyConnect> {
-  // List<Map<String, String?>> allUserData = [];
-  // bool isDone = false;
+class _NearbyConnectState extends State<NearbyConnect> {
+  List<Map<String, String?>> allUserData = [];
+  bool isDone = false;
 
-  // Future<Map<String, String?>> fetchUserData(String uid) async {
-  //   var userData = new Map<String, String?>();
-  //   List<String?> connectedList = [];
-  //
-  //   await FirebaseFirestore.instance
-  //       .collection("users")
-  //       .doc(uid)
-  //       .get()
-  //       .then((DocumentSnapshot snapshot) async {
-  //     userData["uid"] = snapshot["uid"];
-  //     userData["fullname"] = snapshot["fullname"];
-  //     userData["designation"] = snapshot["designation"];
-  //     userData["bio"] = snapshot["bio"];
-  //     userData["imageUrl"] = snapshot["imageUrl"];
-  //     userData["linkedIn"] = snapshot["linkedIn"];
-  //     userData["github"] = snapshot["github"];
-  //     userData["portfolio"] = snapshot["portfolio"];
-  //     userData["twitter"] = snapshot["twitter"];
-  //     // userData["connectedList"] = snapshot["connectedList"];
-  //     var connectedData = snapshot["connectedList"];
-  //     connectedList= List<String?>.from(connectedData);
-  //   });
-  //
-  //   return userData;
-  // }
+  Future<Map<String, String?>> fetchUserData(String uid) async {
+    var userData = new Map<String, String?>();
+    List<String?> connectedList = [];
 
-  // void getallData(List<String> uidList) async {
-  //   List<Map<String, String?>> allData = [];
-  //
-  //   for (var uid in uidList) {
-  //     // Get.snackbar("Uid", uid);
-  //     final alphanumeric = RegExp(r'^[a-zA-Z0-9_]*$');
-  //     print("Before if "+ uid);
-  //     if(alphanumeric.hasMatch(uid) == true){
-  //       print(uid);
-  //       await fetchUserData(uid).then((value) {
-  //         // "^[a-zA-Z0-9_]*$"
-  //
-  //         allData.add(value);
-  //       });
-  //     }
-  //     //print(uid);
-  //
-  //   }
-  //   setState(() {
-  //     isDone = true;
-  //     allUserData = allData;
-  //   });
-  // }
+    await FirebaseFirestore.instance
+        .collection("users")
+        .doc(uid)
+        .get()
+        .then((DocumentSnapshot snapshot) async {
+      userData["uid"] = snapshot["uid"];
+      userData["name"] = snapshot["name"];
+      userData["designation"] = snapshot["designation"];
+      userData["bio"] = snapshot["bio"];
+      userData["imageURL"] = snapshot["imageURL"];
+      userData["linkedin"] = snapshot["linkedin"];
+      userData["github"] = snapshot["github"];
+      userData["portfolio"] = snapshot["portfolio"];
+      userData["twitter"] = snapshot["twitter"];
+      userData["isPrivate"] = snapshot["isPrivate"];
+      // userData["connectedList"] = snapshot["connectedList"];
+      var connectedData = snapshot["connectedList"];
+      connectedList = List<String?>.from(connectedData);
+    });
 
-  // @override
-  // void initState() {
-  //   final cp = context.read<ConnectionProvider>();
-  //   if (isDone == false) {
-  //     getallData(cp.connections);
-  //   }
-  //   super.initState();
-  // }
+    return userData;
+  }
+
+  void getallData(List<String> uidList) async {
+    List<Map<String, String?>> allData = [];
+
+    for (var uid in uidList) {
+      // Get.snackbar("Uid", uid);
+      final alphanumeric = RegExp(r'^[a-zA-Z0-9_]*$');
+      print("Before if " + uid);
+      if (alphanumeric.hasMatch(uid) == true) {
+        print(uid);
+        await fetchUserData(uid).then((value) {
+          // "^[a-zA-Z0-9_]*$"
+
+          allData.add(value);
+        });
+      }
+      //print(uid);
+
+    }
+    setState(() {
+      isDone = true;
+      allUserData = allData;
+    });
+  }
+
+  @override
+  void initState() {
+    final cp = context.read<ConnectionProvider>();
+    if (isDone == false) {
+      getallData(cp.connections);
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final cp = ref.watch(connectionProvider);
-    final _databaseProvider = ref.watch(databaseProvider);
-    // final _userdata = ref.watch(nearbyConnectionsProvider);
-    print("-------Connection IDs-------");
-    print(cp.connections);
-    List<UserModel>? nearbyUsers = [];
-
-    // nearbyUsers = _userdatavoid
-    void fetchData() async {
-      nearbyUsers = await _databaseProvider.getNearbyData(cp.connections);
-    }
-
-    @override
-    void initState() {
-      print("----------------------------------------");
-      fetchData();
-      print(nearbyUsers!.length);
-      super.initState();
-    }
-
+    print("Outside check -> ");
+    print(allUserData.length);
     return Scaffold(
         drawer: const Menu(),
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(screenHeight! * 0.12),
-          child: CustomAppbar(context),
+        appBar: AppBar(
+          toolbarHeight: Get.height * 0.12,
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.transparent,
+          shadowColor: primarybgcolor,
+          elevation: 0.0,
+          leading: Builder(
+            builder: (context) => IconButton(
+              splashRadius: 1,
+              padding: const EdgeInsets.fromLTRB(30, 40, 0, 25),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              icon: FaIcon(
+                FontAwesomeIcons.bars,
+                color: arrowcolor,
+              ),
+              alignment: Alignment.centerLeft,
+            ),
+          ),
         ),
         body: Container(
           margin: EdgeInsets.fromLTRB(30, 0, 30, 25),
@@ -119,86 +119,94 @@ class _NearbyConnectState extends ConsumerState<NearbyConnect> {
                   fontWeight: FontWeight.w600,
                 ),
               ),
-              SizedBox(height: screenHeight! * .02),
+              SizedBox(height: Get.height * .02),
               SingleChildScrollView(
                 child: Container(
-                    height: screenHeight! * 0.6,
+                    height: Get.height * 0.6,
                     //height: Get.height*0.5,
                     child: ListView.builder(
-                        itemCount: nearbyUsers!.length,
+                        itemCount: allUserData.length,
                         itemBuilder: (context, i) {
                           //return Connect(userdata["fullname"], userdata["designation"]);
 
-                          return Connect(nearbyUsers![i], nearbyUsers![i].name,
-                              nearbyUsers![i].designation, context);
+                          return Connect(
+                              allUserData[i],
+                              allUserData[i]["fullname"],
+                              allUserData[i]["designation"]);
                         })),
               ),
             ],
           ),
         ));
   }
-}
 
-Widget Connect(allUserData, name, designation, context) {
-  return InkWell(
-      onTap: () {
-        ProfileDialog(allUserData, context);
-      },
-      child: Container(
-          alignment: Alignment.center,
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          margin: EdgeInsets.only(bottom: 20),
-          height: screenHeight! * 0.15,
-          decoration: BoxDecoration(
-            color: Color(0xffEEF7FE),
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    name,
-                    //textAlign: TextAlign.start,
-                    style: TextStyle(
-                      letterSpacing: 1,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
+  Widget Connect(allUserData, name, designation) {
+    final sp = context.read<SignInProvider>();
+    return InkWell(
+        onTap: () {
+          ProfileDialog(allUserData, context);
+        },
+        child: Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            margin: EdgeInsets.only(bottom: 20),
+            height: Get.height * 0.15,
+            decoration: BoxDecoration(
+              color: Color(0xffEEF7FE),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      name,
+                      //textAlign: TextAlign.start,
+                      style: TextStyle(
+                        letterSpacing: 1,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: screenHeight! * 0.01,
-                  ),
-                  Text(
-                    designation,
-                    //textAlign: TextAlign.start,
-                    style: TextStyle(
-                      letterSpacing: 1,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w300,
+                    SizedBox(
+                      height: Get.height * 0.01,
                     ),
-                  ),
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Align(
+                    Text(
+                      designation,
+                      //textAlign: TextAlign.start,
+                      style: TextStyle(
+                        letterSpacing: 1,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Align(
                       alignment: Alignment.centerRight,
                       child: IconButton(
                         onPressed: () async {
-                          // await sp.addConnection(allUserData["uid"]);
-                          // Get.snackbar("Connection Added", allUserData["fullname"]);
+                          await sp.addConnection(allUserData["uid"]);
+                          Get.snackbar(
+                              "Connection Added", allUserData["fullname"]);
                         },
-                        icon: Icon(Icons.person_add_alt_1_rounded),
-                      )),
-                ],
-              )
-            ],
-          )));
+                        icon: FaIcon(
+                          FontAwesomeIcons.userPlus,
+                          color: arrowcolor,
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            )));
+  }
 }
