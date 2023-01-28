6a9c63f43723d66615bd46b6317930db8f266e3f
priyasu-cx <codexposer.priyasu25@gmail.com>
2023-01-29 04:12:32 +0530

    Some Changes

--- lib/NearbyConnectScreen/nearby_connect.dart
+++ lib/NearbyConnectScreen/nearby_connect.dart
@@ -1,91 +1,89 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hacknitr_round2/Models/user_models.dart';
 import 'package:hacknitr_round2/Providers/connection_provider.dart';
-import 'package:hacknitr_round2/Providers/database_provider.dart';
-import 'package:hacknitr_round2/Services/database_service.dart';
 import 'package:hacknitr_round2/utils/size_config.dart';
 import 'package:hacknitr_round2/widgets/appbar.dart';
 import 'package:hacknitr_round2/widgets/drawer.dart';
 import 'package:hacknitr_round2/widgets/profile_dialog.dart';
 
-class NearbyConnect extends StatefulWidget {
+class NearbyConnect extends ConsumerStatefulWidget {
   const NearbyConnect({Key? key}) : super(key: key);
 
   @override
-  State<NearbyConnect> createState() => _NearbyConnectState();
+  ConsumerState<NearbyConnect> createState() => _NearbyConnectState();
 }
 
-class _NearbyConnectState extends State<NearbyConnect> {
-  List<Map<String, String?>> allUserData = [];
-  bool isDone = false;
-
-  Future<Map<String, String?>> fetchUserData(String uid) async {
-    var userData = new Map<String, String?>();
-    List<String?> connectedList = [];
-
-    await FirebaseFirestore.instance
-        .collection("users")
-        .doc(uid)
-        .get()
-        .then((DocumentSnapshot snapshot) async {
-      userData["uid"] = snapshot["uid"];
-      userData["name"] = snapshot["name"];
-      userData["designation"] = snapshot["designation"];
-      userData["bio"] = snapshot["bio"];
-      userData["imageURL"] = snapshot["imageURL"];
-      userData["linkedin"] = snapshot["linkedin"];
-      userData["github"] = snapshot["github"];
-      userData["portfolio"] = snapshot["portfolio"];
-      userData["twitter"] = snapshot["twitter"];
-      userData["isPrivate"] = snapshot["isPrivate"];
-      // userData["connectedList"] = snapshot["connectedList"];
-      var connectedData = snapshot["connectedList"];
-      connectedList = List<String?>.from(connectedData);
-    });
-
-    return userData;
-  }
-
-  void getallData(List<String> uidList) async {
-    List<Map<String, String?>> allData = [];
-
-    for (var uid in uidList) {
-      // Get.snackbar("Uid", uid);
-      final alphanumeric = RegExp(r'^[a-zA-Z0-9_]*$');
-      print("Before if " + uid);
-      if (alphanumeric.hasMatch(uid) == true) {
-        print(uid);
-        await fetchUserData(uid).then((value) {
-          // "^[a-zA-Z0-9_]*$"
-
-          allData.add(value);
-        });
-      }
-      //print(uid);
-
-    }
-    setState(() {
-      isDone = true;
-      allUserData = allData;
-    });
-  }
-
-  @override
-  void initState() {
-    final cp = context.read<ConnectionProvider>();
-    if (isDone == false) {
-      getallData(cp.connections);
-    }
-    super.initState();
-  }
+class _NearbyConnectState extends ConsumerState<NearbyConnect> {
+  // List<Map<String, String?>> allUserData = [];
+  // bool isDone = false;
+  //
+  // Future<Map<String, String?>> fetchUserData(String uid) async {
+  //   var userData = new Map<String, String?>();
+  //   List<String?> connectedList = [];
+  //
+  //   await FirebaseFirestore.instance
+  //       .collection("users")
+  //       .doc(uid)
+  //       .get()
+  //       .then((DocumentSnapshot snapshot) async {
+  //     userData["uid"] = snapshot["uid"];
+  //     userData["name"] = snapshot["name"];
+  //     userData["designation"] = snapshot["designation"];
+  //     userData["bio"] = snapshot["bio"];
+  //     userData["imageURL"] = snapshot["imageURL"];
+  //     userData["linkedin"] = snapshot["linkedin"];
+  //     userData["github"] = snapshot["github"];
+  //     userData["portfolio"] = snapshot["portfolio"];
+  //     userData["twitter"] = snapshot["twitter"];
+  //     userData["isPrivate"] = snapshot["isPrivate"];
+  //     // userData["connectedList"] = snapshot["connectedList"];
+  //     var connectedData = snapshot["connectedList"];
+  //     connectedList = List<String?>.from(connectedData);
+  //   });
+  //
+  //   return userData;
+  // }
+  //
+  // void getallData(List<String> uidList) async {
+  //   List<Map<String, String?>> allData = [];
+  //
+  //   for (var uid in uidList) {
+  //     // Get.snackbar("Uid", uid);
+  //     final alphanumeric = RegExp(r'^[a-zA-Z0-9_]*$');
+  //     print("Before if " + uid);
+  //     if (alphanumeric.hasMatch(uid) == true) {
+  //       print(uid);
+  //       await fetchUserData(uid).then((value) {
+  //         // "^[a-zA-Z0-9_]*$"
+  //
+  //         allData.add(value);
+  //       });
+  //     }
+  //     //print(uid);
+  //
+  //   }
+  //   setState(() {
+  //     isDone = true;
+  //     allUserData = allData;
+  //   });
+  // }
+  //
+  // @override
+  // void initState() {
+  //   final cp = ref.watch(connectionProvider);
+  //   if (isDone == false) {
+  //     getallData(cp.connections);
+  //   }
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final cp = ref.watch(connectionProvider);
     // final _userdata = ref.watch(nearbyConnectionsProvider);
     print("-------Connection IDs-------");
-    print(cp.connections);
+    // print(cp.connections);
     List<UserModel>? nearbyUsers = [];
 
     // nearbyUsers = _userdata
@@ -114,11 +112,11 @@ class _NearbyConnectState extends State<NearbyConnect> {
                     height: screenHeight! * 0.6,
                     //height: Get.height*0.5,
                     child: ListView.builder(
-                        itemCount: nearbyUsers.length,
+                        itemCount: allUserData.length,
                         itemBuilder: (context, i) {
                           //return Connect(userdata["fullname"], userdata["designation"]);
 
-                          return Connect(user: nearbyUsers[i], name: nearbyUsers[i].name, designation: nearbyUsers[i].designation!);
+                          return Connect(user: allUserData[i], name: allUserData[i]["name"]!, designation: allUserData[i]["designation"]!);
                         })),
               ),
             ],
@@ -128,7 +126,7 @@ class _NearbyConnectState extends State<NearbyConnect> {
 }
 
 class Connect extends ConsumerWidget {
-  final UserModel user;
+  final Map user;
   final String name;
   final String designation;
   
