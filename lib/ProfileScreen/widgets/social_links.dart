941df0b45dd0a5f4e71ee9d819794b822323f705
shubhayu-64 <majumdarshubhayu@gmail.com>
2023-01-28 23:33:20 +0530

    zz

--- lib/ProfileScreen/widgets/social_links.dart
+++ lib/ProfileScreen/widgets/social_links.dart
@@ -1,88 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hacknitr_round2/Models/user_models.dart';
+import 'package:hacknitr_round2/Providers/database_provider.dart';
+import 'package:hacknitr_round2/utils/assets.dart';
 import 'package:hacknitr_round2/utils/size_config.dart';
+
+class SocialWidget extends ConsumerWidget {
+  int index;
+  ImageAsset image;
+  String body;
+  SocialWidget(
+      {super.key,
+      required this.index,
+      required this.image,
+      required this.body});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final _userState = ref.watch(userDetailsProvider);
+    return Container();
+  }
+}
 
 Widget social(context, index, image, text, link) {
   openDialog(image, text, index) => showDialog(
-    context: context,
-    builder: (context) {
-      // final sp = context.read<SignInProvider>();
-      // UserModel userdetails = UserModel();
-      final myController = TextEditingController();
-      return Dialog(
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20)),
-          child: Stack(
-            clipBehavior: Clip.none,
-            alignment: Alignment.topCenter,
-            children: [
-              Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  height: 200,
-                  child: Padding(
-                      padding: const EdgeInsets.fromLTRB(10, 70, 10, 10),
-                      child: Column(children: [
-                        Text(
-                          text + " Link",
-                          style: TextStyle(
-                            letterSpacing: 1,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        TextFormField(
-                          controller: myController,
-                          autofocus: true,
-                          decoration: InputDecoration(
-                              hintText: index == 1
-                                  ? "https://www.linkedin.com/"
-                                  : index == 2
-                                  ? "https://github.com/"
-                                  : index == 3
-                                  ? "https://yoursite.com/"
-                                  : "https://twitter.com/"),
-                        ),
-                        TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-
-                              /// TODO: Add link to database
-                              // index == 1
-                              //     ? sp.setLinkedIn(myController.text)
-                              //     : index == 2
-                              //     ? sp.setGithub(myController.text)
-                              //     : index == 3
-                              //     ? sp.setPortfolio(
-                              //     myController.text)
-                              //     : sp.setTwitter(
-                              //     myController.text);
-                              // Get.snackbar("Link Submitted", "");
-                            },
-                            child: Text(
-                              'SUBMIT',
+        context: context,
+        builder: (context) {
+          // final sp = context.read<SignInProvider>();
+          // UserModel userdetails = UserModel();
+          final myController = TextEditingController();
+          return Dialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              child: Stack(
+                clipBehavior: Clip.none,
+                alignment: Alignment.topCenter,
+                children: [
+                  Container(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      height: 200,
+                      child: Padding(
+                          padding: const EdgeInsets.fromLTRB(10, 70, 10, 10),
+                          child: Column(children: [
+                            Text(
+                              text + " Link",
                               style: TextStyle(
                                 letterSpacing: 1,
-                                fontSize: 20,
-                                fontWeight: FontWeight.w800,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
                               ),
-                            ))
-                      ]))),
-              Positioned(
-                child: CircleAvatar(
-                  backgroundColor: Color(0xffced5ff),
-                  radius: 50,
-                  child: Image.asset(
-                    image,
-                    height: 40,
-                    width: 40,
-                  ),
-                ),
-                top: -50,
-              )
-            ],
-          ));
-    },
-  );
+                            ),
+                            TextFormField(
+                              controller: myController,
+                              autofocus: true,
+                              decoration: InputDecoration(
+                                  hintText: index == 1
+                                      ? "https://www.linkedin.com/"
+                                      : index == 2
+                                          ? "https://github.com/"
+                                          : index == 3
+                                              ? "https://yoursite.com/"
+                                              : "https://twitter.com/"),
+                            ),
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+
+                                  /// TODO: Add link to database
+                                  // index == 1
+                                  //     ? sp.setLinkedIn(myController.text)
+                                  //     : index == 2
+                                  //     ? sp.setGithub(myController.text)
+                                  //     : index == 3
+                                  //     ? sp.setPortfolio(
+                                  //     myController.text)
+                                  //     : sp.setTwitter(
+                                  //     myController.text);
+                                  // Get.snackbar("Link Submitted", "");
+                                },
+                                child: Text(
+                                  'SUBMIT',
+                                  style: TextStyle(
+                                    letterSpacing: 1,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w800,
+                                  ),
+                                ))
+                          ]))),
+                  Positioned(
+                    child: CircleAvatar(
+                      backgroundColor: Color(0xffced5ff),
+                      radius: 50,
+                      child: Image.asset(
+                        image,
+                        height: 40,
+                        width: 40,
+                      ),
+                    ),
+                    top: -50,
+                  )
+                ],
+              ));
+        },
+      );
 
   return GestureDetector(
       onTap: () {
@@ -99,7 +119,6 @@ Widget social(context, index, image, text, link) {
         child: Stack(
           alignment: Alignment.center,
           children: [
-
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -120,8 +139,8 @@ Widget social(context, index, image, text, link) {
                   ),
                 ),
               ],
-            )],
+            )
+          ],
         ),
-      )
-  );
+      ));
 }
